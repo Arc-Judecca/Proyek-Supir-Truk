4c6853478d95705ede8c0128c0ffebb947332df5
Arc-Judecca <gilangfibarkah125@gmail.com>
2024-03-13 12:36:13 +0700

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@ use Illuminate\Support\Facades\Auth;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
+| Here is where you can register web routes for your application.
+| These routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
@@ -22,11 +22,15 @@ Route::get('/', function () {
 Auth::routes(['register' => false]);
 
 Route::middleware(['auth'])->group(function () {
+    Route::get('/', function () {
+        return redirect()->route('supir.index');
+    });
+
     Route::get('/supir', [SupirController::class, 'index'])->name('supir.index');
     Route::get('/supir/create', [SupirController::class, 'create'])->name('supir.create');
     Route::post('/supir', [SupirController::class, 'store'])->name('supir.store');
+    Route::delete('/supir/{id}', [SupirController::class, 'destroy'])->name('supir.destroy');
 });
-Route::delete('/supir/{id}', [SupirController::class, 'destroy'])->name('supir.destroy');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
